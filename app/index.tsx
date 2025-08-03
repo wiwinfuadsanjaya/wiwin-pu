9cb22f243ba193d2f6c0b65b21cc2a91a49ecb6c
wiwinfuadsanjaya <140473100+wiwinfuadsanjaya@users.noreply.github.com>
2025-08-02 12:04:10 -1200

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,7 @@
 import React from 'react';
-import { useFonts } from 'expo-font';
 import { Text, StyleSheet, ScrollView, View } from 'react-native';
 
 export default function FontScreen() {
-  const [fontsLoaded] = useFonts({
-    // 5 static fonts
-    'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
-    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
-    'Poppins-ExtraBold': require('../assets/fonts/Poppins-ExtraBold.ttf'),
-    'Poppins-ExtraLight': require('../assets/fonts/Poppins-ExtraLight.ttf'),
-    'SpaceMono-Regular': require('../assets/fonts/SpaceMono-Regular.ttf'),
-    // 5 variable fonts
-    'Montserrat-Variable': require('../assets/fonts/Montserrat-VariableFont_wght.ttf'),
-    'Montserrat-Italic-Variable': require('../assets/fonts/Montserrat-Italic-VariableFont_wght.ttf'),
-    'OpenSans-Variable': require('../assets/fonts/OpenSans-VariableFont_wdth,wght.ttf'),
-    'OpenSans-Italic-Variable': require('../assets/fonts/OpenSans-Italic-VariableFont_wdth,wght.ttf'),
-    'Bitcount-Variable': require('../assets/fonts/Bitcount-VariableFont_CRSV,ELSH,ELXP,slnt,wght.ttf'),
-  });
-
-  if (!fontsLoaded) {
-    return <Text>Loading Fonts...</Text>;
-  }
 
   // Data mahasiswa (nama dan stambuk)
   const mahasiswa = [
